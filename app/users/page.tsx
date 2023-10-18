63cdf87751c1b578ef1e8b45debb6f070e27b1e0
MRAlirad <m.r.alirad75@gmail.com>
2023-10-18 21:39:48 +0330

    caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,21 +1,26 @@
 interface User {
-    id: number;
-    name: string;
+	id: number;
+	name: string;
 }
 
 const UserPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users : User[] = await res.json();
-	return(
-        <>
-            <h1>Users</h1>
-            <ul>
-                {
-                    users.map(user => <li key={user.id}> {user.name}</li>)
-                }
-            </ul>
-        </>
-    )
+	const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+		// cache: 'no-store',
+		next: {
+			revalidate: 10,
+		},
+	});
+	const users: User[] = await res.json();
+	return (
+		<>
+			<h1>Users</h1>
+			<ul>
+				{users.map(user => (
+					<li key={user.id}> {user.name}</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default UserPage;
