aa1ce36f5d57225ab6052d0c1cecd5723f810bae
MRAlirad <m.r.alirad75@gmail.com>
2023-10-18 21:04:37 +0330

    client/server componets

---
+++ app/components/Button.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react';
+
+interface Props {
+	text: string;
+    onClick : ()=> void;
+}
+
+const Button = ({ text, onClick }: Props) => {
+	return <button className='btn' onClick={onClick}>{text}</button>;
+};
+
+export default Button;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import Button from './Button'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <Button text='addToCard' onClick={()=> console.log('wow')} />
+    </div>
+  )
+}
+
+export default ProductCard
