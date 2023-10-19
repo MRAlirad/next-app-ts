2182d3edb4adca4bebef9817fbfdf4ad372184e1
MRAlirad <m.r.alirad75@gmail.com>
2023-10-19 12:49:23 +0330

    user table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,25 +1,11 @@
-interface User {
-	id: number;
-	name: string;
-}
+import UserTable from './UserTable';
 
 const UserPage = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-		// cache: 'no-store',
-		next: {
-			revalidate: 10,
-		},
-	});
-	const users: User[] = await res.json();
 	return (
 		<>
 			<h1>Users</h1>
 			<p>{new Date().toLocaleTimeString()}</p>
-			<ul>
-				{users.map(user => (
-					<li key={user.id}> {user.name}</li>
-				))}
-			</ul>
+			<UserTable />
 		</>
 	);
 };
