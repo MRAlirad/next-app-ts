9b5c610babae82eab259236375fd7b1425fb6121
MRAlirad <m.r.alirad75@gmail.com>
2023-10-19 16:57:13 +0330

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+	return <div>this page does not fount , dude?!?!?!</div>;
+};
+
+export default NotFound;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>the user you are looking for does not exist!!!</div>
+  )
+}
+
+export default NotFound
