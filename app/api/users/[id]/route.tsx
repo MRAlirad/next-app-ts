462798acc1d5f91944e4318b722eb7a485ded68b
MRAlirad <m.r.alirad75@gmail.com>
2023-10-20 21:59:44 +0330

    update object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,13 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 interface Props {
-    params : {
-        id: number,
-    }
+	params: {
+		id: number;
+	};
 }
 
-export function GET(request: NextRequest, {params} : Props) {
-    if(params.id > 10)
-        return NextResponse.json({error : 'User Not Found'}, {status : 404});
-    return NextResponse.json({id: 1, name : 'Mohammad'});
+export function GET(request: NextRequest, { params }: Props) {
+	if (params.id > 10) return NextResponse.json({ error: 'User Not Found' }, { status: 404 });
+	return NextResponse.json({ id: 1, name: 'Mohammad' });
+}
+
+export async function PUT(request: NextRequest, { params }: Props) {
+	const body = await request.json();
+	if (!body.name) return NextResponse.json({ error: 'name is required' }, { status: 400 });
+	if (params.id > 10) return NextResponse.json({ error: 'user not found' }, { status: 404 });
+
+	return NextResponse.json({ id: 1, name: body.name });
 }
