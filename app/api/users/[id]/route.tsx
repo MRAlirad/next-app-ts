815dc751e7b9e02d1d9027604596e0a1acb640fc
MRAlirad <m.r.alirad75@gmail.com>
2023-10-20 22:16:24 +0330

    validation with zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import schema from '../schema';
 
 interface Props {
 	params: {
@@ -13,7 +14,8 @@ export function GET(request: NextRequest, { params }: Props) {
 
 export async function PUT(request: NextRequest, { params }: Props) {
 	const body = await request.json();
-	if (!body.name) return NextResponse.json({ error: 'name is required' }, { status: 400 });
+	const validation = schema.safeParse(body);
+	if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
 	if (params.id > 10) return NextResponse.json({ error: 'user not found' }, { status: 404 });
 
 	return NextResponse.json({ id: 1, name: body.name });
