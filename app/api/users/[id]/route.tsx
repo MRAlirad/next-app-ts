3b3b23827d6dec524e77ff74314d3f1f9fb8306a
MRAlirad <m.r.alirad75@gmail.com>
2023-10-20 21:25:41 +0330

    get object

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+    params : {
+        id: number,
+    }
+}
+
+export function GET(request: NextRequest, {params} : Props) {
+    if(params.id > 10)
+        return NextResponse.json({error : 'User Not Found'}, {status : 404});
+    return NextResponse.json({id: 1, name : 'Mohammad'});
+}
