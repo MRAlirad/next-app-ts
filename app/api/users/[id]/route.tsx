d2993b96cb5772b4d16f752ba74bb03d721a5eb9
MRAlirad <m.r.alirad75@gmail.com>
2023-10-22 19:05:09 +0330

    update a user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -22,9 +22,22 @@ export async function PUT(request: NextRequest, { params }: Props) {
 	const body = await request.json();
 	const validation = schema.safeParse(body);
 	if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
-	if (params.id > 10) return NextResponse.json({ error: 'user not found' }, { status: 404 });
+	const user = await prisma.user.findUnique({
+		where: {id: parseInt(params.id)}
+	})
+	if (!user) return NextResponse.json({ error: 'user not found' }, { status: 404 });
 
-	return NextResponse.json({ id: 1, name: body.name });
+	const updatedUser = await prisma.user.update({
+		where: {
+			id: user.id,
+		},
+		data : {
+			name : body.name,
+			email: body.email,
+		}
+	})
+
+	return NextResponse.json(updatedUser);
 }
 
 export async function Delete(request: NextRequest, { params }: Props) {
