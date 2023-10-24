bfb548682ddc1456fe75288e177e7447888c50ec
MRAlirad <m.r.alirad75@gmail.com>
2023-10-24 13:06:02 +0330

    optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Link from 'next/link';
-import ProductCard from './components/ProductCard';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
+import Image from 'next/image';
+import snow from '@/public/images/Snow.jpg';
 
 export default async function Home() {
-	const session = await getServerSession(authOptions);
-
 	return (
-		<main>
-			<h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-			<Link href={'/users'}> users </Link>
-			<ProductCard />
+		<main className='relative h-screen'>
+			<Image
+				src='https://bit.ly/react-cover'
+				alt='Snow'
+				fill
+				className='object-cover'
+				sizes='(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw'
+				quality={100}
+				priority
+			/>
 		</main>
 	);
 }
