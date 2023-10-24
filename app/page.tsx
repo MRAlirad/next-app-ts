167dbdbd8bc1128ae94616a294d7c4df975dd630
MRAlirad <m.r.alirad75@gmail.com>
2023-10-24 15:13:47 +0330

    lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
-import Image from 'next/image';
-import snow from '@/public/images/Snow.jpg';
-import { Metadata } from 'next';
+'use client'
 
-export default async function Home() {
+// import Image from 'next/image';
+// import snow from '@/public/images/Snow.jpg';
+import { Metadata } from 'next';
+import { useState } from 'react';
+// import dynamic from 'next/dynamic';
+import _ from 'loadash';
+// const HeavyComponent = dynamic(
+// 	()=> import('./components/HeavyComponent'),
+// 	{
+// 		ssr : false,
+// 		loading: ()=> <p>Loading...</p>
+// 	}
+// )
+
+export default function Home() {
+	// const [isVisible, setVisible] = useState(false)
 	return (
 		<main className='relative h-screen'>
 			<h1 className='font-poppins'>Hello World</h1>
-			<Image
+			<button onClick={()=> {
+				// const users = [
+				// 	{name: 'c'},
+				// 	{name: 'a'},
+				// 	{name: 'b'},
+				// ];
+				// const sorted = _.orderBy(users, ['name']);
+				// console.log(sorted);
+			}}>Show</button>
+			{/* {isVisible && <HeavyComponent />} */}
+			{/* <Image
 				src='https://bit.ly/react-cover'
 				alt='Snow'
 				fill
@@ -14,7 +37,7 @@ export default async function Home() {
 				sizes='(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw'
 				quality={100}
 				priority
-			/>
+			/> */}
 		</main>
 	);
 }
